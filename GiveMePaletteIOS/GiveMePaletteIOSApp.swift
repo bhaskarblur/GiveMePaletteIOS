3bcd9b8292f6ee1439718321f95fd205578cbfa8
Forest Defenders Club NFTs <bvkaura@gmail.com>
2023-11-08 08:06:32 +0530

    Initial Commit

---
+++ GiveMePaletteIOS/GiveMePaletteIOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  GiveMePaletteIOSApp.swift
+//  GiveMePaletteIOS
+//
+//  Created by SoundWave2847 on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GiveMePaletteIOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
